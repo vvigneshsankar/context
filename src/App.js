1d9e60c0f30943e53aefd6305b8fa59e624f81a6
Vignesh <vvigneshsankar@gmail.com>
2019-04-02 10:11:29 +0530

    files addded

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const MyContext=React.createContext();
+  
+class MyProvider extends Component{
+  state={
+    name:'Raju',
+    age:32,
+  }
+  
+  render(){
+    return(
+      <MyContext.Provider value={{state:this.state,
+     growYearOlder:()=>this.setState({
+        age:this.state.age+1})}}>
+        {this.props.children}
+        
+        })
+      </MyContext.Provider>
+    )
+  }
+}
+
+const Family=(props)=>(
+  <div className="family">
+  <Person />
+  </div>
+)
+ 
+class Person extends Component {
+  render() { 
+    return(
+    <div className="Person">
+   <MyContext.Consumer>
+     {(context)=>(
+       <React.Fragment>
+       <h1>Age:{context.state.age}</h1>
+       <button onClick={context.growYearOlder}>Grow</button>
+       </React.Fragment>
+     )
+
+     }
+   </MyContext.Consumer>
+    </div>
+    )
+  }
+}
+ 
 
 class App extends Component {
   render() {
     return (
+      <MyProvider>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+     <Family/>
       </div>
+      </MyProvider>
     );
   }
 }
